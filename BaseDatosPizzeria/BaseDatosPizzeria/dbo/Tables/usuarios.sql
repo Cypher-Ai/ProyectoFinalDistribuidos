930a4879f5b52c70645248f5601ec0066d6360c9
Kevin <kevin.santiago.27@unsch.edu.pe>
2021-11-29 11:39:08 -0500

    correcion de tipo de datos a la base de usuariod

--- BaseDatosPizzeria/BaseDatosPizzeria/dbo/Tables/usuarios.sql
+++ BaseDatosPizzeria/BaseDatosPizzeria/dbo/Tables/usuarios.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[usuarios] (
-    [idusuario]          INT          IDENTITY (1, 1) NOT NULL,
-    [nombres]            VARCHAR (30) NOT NULL,
-    [apellidos]          VARCHAR (30) NOT NULL,
-    [nrodni]             VARCHAR (30) NOT NULL,
-    [telefono]           VARCHAR (30) NOT NULL,
-    [correo]             VARCHAR (30) NOT NULL,
-    [fechanacimiento]    VARCHAR (30) NOT NULL,
-    [direccion]          VARCHAR (30) NOT NULL,
-    [contraseniausuario] VARCHAR (30) NOT NULL,
-    PRIMARY KEY CLUSTERED ([idusuario] ASC)
+    [Id]              INT       IDENTITY (1, 1) NOT NULL,
+    [nombre]          CHAR (50) NULL,
+    [apellidos]       CHAR (50) NULL,
+    [telefono]        INT       NULL,
+    [nrodni]          INT       NULL,
+    [correo]          CHAR (50) NULL,
+    [fechanacimiento] CHAR (20) NULL,
+    [direccion]       CHAR (50) NULL,
+    [contraseña]      CHAR (30) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
