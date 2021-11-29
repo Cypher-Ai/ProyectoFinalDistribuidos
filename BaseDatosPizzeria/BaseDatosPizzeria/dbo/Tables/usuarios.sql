fdb6d1947f5703de0d4da1a7babc38caf770bcb4
Kevin <kevin.santiago.27@unsch.edu.pe>
2021-11-29 11:01:04 -0500

    Base de datos de usuarios incorporada

---
+++ BaseDatosPizzeria/BaseDatosPizzeria/dbo/Tables/usuarios.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[usuarios] (
+    [idusuario]          INT          IDENTITY (1, 1) NOT NULL,
+    [nombres]            VARCHAR (30) NOT NULL,
+    [apellidos]          VARCHAR (30) NOT NULL,
+    [nrodni]             VARCHAR (30) NOT NULL,
+    [telefono]           VARCHAR (30) NOT NULL,
+    [correo]             VARCHAR (30) NOT NULL,
+    [fechanacimiento]    VARCHAR (30) NOT NULL,
+    [direccion]          VARCHAR (30) NOT NULL,
+    [contraseniausuario] VARCHAR (30) NOT NULL,
+    PRIMARY KEY CLUSTERED ([idusuario] ASC)
+);
+
